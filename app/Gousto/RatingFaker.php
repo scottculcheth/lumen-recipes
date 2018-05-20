9f484f7fe3a992aab90fad596443ce3205969180
Scott Culcheth <>
2018-05-20 22:44:15 +0000

    Faker fixed

--- app/Gousto/RatingFaker.php
+++ app/Gousto/RatingFaker.php
@@ -2,18 +2,19 @@
 
 namespace App\Gousto;
 
+use Faker\Factory as Faker;
 
 class RatingFaker
 {
 
     public static function createFakeRating()
     {
-        $faker = Faker\Factory::create();
+        $faker = Faker::create();
 
         $rating = [];
         $rating['id'] = $faker->randomNumber(3);
         $rating['recipe_id'] = $faker->randomNumber(3);
-        $rating['user_id'] = $faker->randomNumber(10);
+        $rating['user_id'] = $faker->randomNumber(3);
         $rating['rating'] = $faker->randomElement([1,2,3,4,5]);
         $rating['created_at'] = $faker->date('Y-m-d H:i:s');
         $rating['updated_at'] = $faker->date('Y-m-d H:i:s');
--- app/Gousto/RecipeFaker.php
+++ app/Gousto/RecipeFaker.php
@@ -2,12 +2,14 @@
 
 namespace App\Gousto;
 
+use Faker\Factory as Faker;
+
 class RecipeFaker
 {
 
     public static function createFakeRecipe()
     {
-        $faker = Faker\Factory::create();
+        $faker = Faker::create();
 
         $recipe = [];
         $recipe['id'] = $faker->randomNumber(3);
@@ -17,15 +19,15 @@ class RecipeFaker
         $recipe['title'] = $faker->words(4, true);
         $recipe['slug'] = str_replace(' ', '-', $recipe['title']);
         $recipe['short_title'] = '';
-        $recipe['marketing_description'] = $faker->test(200);
+        $recipe['marketing_description'] = $faker->text(200);
         $recipe['calories_kcal'] = $faker->randomNumber(3);
         $recipe['protein_grams'] = $faker->randomNumber(2);
         $recipe['fat_grams'] = $faker->randomNumber(2);
         $recipe['carbs_grams'] = $faker->randomNumber(2);
-        $recipe['bulletpoint1'] = $faker->test(200);
-        $recipe['bulletpoint2'] = $faker->test(200);
-        $recipe['bulletpoint3'] = $faker->test(200);
-        $recipe['recipe_diet_type_id']; $faker->randomElement(['meat', 'fish', 'vegetarian']);
+        $recipe['bulletpoint1'] = $faker->text(200);
+        $recipe['bulletpoint2'] = $faker->text(200);
+        $recipe['bulletpoint3'] = $faker->text(200);
+        $recipe['recipe_diet_type_id'] = $faker->randomElement(['meat', 'fish', 'vegetarian']);
         $recipe['season'] = 'all';
         $recipe['base'] = $faker->randomElement(['noodles', 'pasta', 'beans']);
         $recipe['protein_source'] = $faker->randomElement(['beef', 'seafood', 'pork', 'cheese', 'chicken', 'eggs']);
@@ -34,7 +36,7 @@ class RecipeFaker
         $recipe['equipment_needed'] = $faker->randomElement(['None', 'Mortar and Pestle', 'Appetite']);
         $recipe['origin_country'] = 'Great Britain';
         $recipe['recipe_cuisine'] = $faker->randomElement(['asian', 'british', 'italian', 'mediterranean', 'mexican']);
-        $recipe['in_your_box'] = $faker->test(200);
+        $recipe['in_your_box'] = $faker->text(200);
         $recipe['gousto_reference'] = $faker->randomNumber(3);
 
         return $recipe;
